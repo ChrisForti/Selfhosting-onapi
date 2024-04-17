33457c450372f6d662f67f4e30d2734519136aca
Christopher Forti <christopher.forti.79@gmail.com>
2024-04-16 22:49:23 -0400

    This is far as I got -f is throwing an error so I am pushing my code.

--- dockers-install.sh
+++ dockers-install.sh
@@ -1,24 +1,21 @@
+# Update
+sudo apt update
+
+# Installing curl
 if (which curl)
 then 
   echo "curl already installed"
 else
   echo "Installing curl"
-  sudo apt update
   sudo apt install curl
 fi
 
-if (test -f /etc/apt/keyrings/docker.asc)
+# Adding, and configuration of gpg keyring
+if [ -f /etc/apt/keyrings/docker.asc ]
 then 
-  echo "Keyrings already present"
+  echo "gpg keyring alread configured"
 else 
-  echo "Creating keyrings"
-  sudo install -m 0755 -d /etc/apt/keyrings
+  echo "configuring gpg keyring"
+  sudo install -m 0755 -d /etc/apt/keyrings curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc chmod a+r /etc/apt/keyrings/docker.asc
 fi
 
-if (which gpg)
-then 
-  echo "Gpg alraed installed"
-else
-  echo "Installing, and configuring gpg"
-  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-fi
