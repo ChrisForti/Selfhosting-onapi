77c26d741676e3e05e6bc266b4e09acedb536ed5
Christopher Forti <christopher.forti.79@gmail.com>
2024-04-17 21:55:46 -0400

    Put the ca-certificates code block back, but stii getting error, It's in the comments.

--- dockers-install.sh
+++ dockers-install.sh
@@ -1,9 +1,18 @@
 # Update
 sudo apt update
 
+# Installing ca-certificates
+if (which ca-certificates)
+then
+  echo "ca-certificates already installed"
+else
+  echo "Installing ca-certificates"
+  sudo apt install ca-certificates
+fi
+
 # Installing curl
 if (which curl)
-then 
+then
   echo "curl already installed"
 else
   echo "Installing curl"
@@ -11,11 +20,10 @@ else
 fi
 
 # Adding, and configuration of gpg keyring
-if [ -f /etc/apt/keyrings/docker.asc ]
-then 
-  echo "gpg keyring alread configured"
-else 
+if (test -f /usr/bin/keyring/docker.asc)
+then
+  echo "gpg keyring exists, and configured"
+else
   echo "configuring gpg keyring"
-  sudo apt install -m 0755 -d /etc/apt/keyrings curl -fssL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc chmod a+r /etc/apt/keyrings/docker.asc
+  sudo apt install -m 0755 -d /etc/apt/keyrings curl-fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc chmod a+r /etc/apt/keyrings/docker.asc
 fi
-
