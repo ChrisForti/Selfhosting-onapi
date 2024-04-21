3ff302c7636f47eeaba4680e0994477a5c4ff23d
Christopher Forti <christopher.forti.79@gmail.com>
2024-04-20 21:31:55 -0400

    I think I got it.

--- dockers-install.sh
+++ dockers-install.sh
@@ -28,21 +28,19 @@ else
   sudo install -m 0755 -d /etc/apt/keyrings
 fi
 
-# Adding gpg keyring
+# Adding a gpg keyring
 if (which gpg)
 then
   echo "gpg keyring exists, and configured"
 else
   echo "configuring gpg keyring"
-  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo -H gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 fi
- 
-# 
-if (stat -c "%a" /etc/bin/keyrings/docker.asc)
+
+if (test -f /etc/bin/keyrings/docker.gpg)
 then
   echo " gpg keyring already configured"
 else
   echo "configuring gpg keyrings"
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
 fi
-
