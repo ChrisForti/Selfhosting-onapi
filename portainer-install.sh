5b788edb0e65578378ec83dcab1a9e74c6fc0756
Christopher Forti <christopher.forti.79@gmail.com>
2024-04-30 21:02:37 -0400

    Updated run command for portainer container.

--- portainer-install.sh
+++ portainer-install.sh
@@ -1,4 +1,8 @@
 # Portainer is a web interface for docker
 docker volume create portainer_data
 
+# If local environment is linux
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+# If local environment is a mac
 docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
